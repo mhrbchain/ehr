c4b13605b27abfa957ff618ebb90bd735a73714a
Siris <29503596+casllux@users.noreply.github.com>
2019-08-02 13:44:23 +0545

    Added Registration for Patient and Provider

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,5 +5,5 @@ var Provider = artifacts.require("Provider");
 module.exports = function(deployer) {
     deployer.deploy(EHR);
     deployer.deploy(Provider, "0x9ffF32165225148F1D583225Ee6250b185FE4808");
-    deployer.deploy(Patient, "Patient1 ", "Patient1 ", "1999/4/4", "Ktm", "0x9ffF32165225148F1D583225Ee6250b185FE4808");
+    deployer.deploy(Patient, "0x9ffF32165225148F1D583225Ee6250b185FE4808");
 }
